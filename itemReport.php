daaa9bc3cf10d479b6195227451c84b4e8cc6884
asyadnazeem <asyadnazeem@gmail.com>
2023-07-29 22:23:56 +0530

    report generate works functional aspect is over

--- itemReport.php
+++ itemReport.php
@@ -1,5 +1,40 @@
+<?php
+global $mysqli;
+include "connection.php";?>
+
+
+<?php
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // SQL query to retrieve the item details for the report
+    $sql = "SELECT item_name, item_category, item_subcategory, SUM(quantity) AS total_quantity
+            FROM item
+            GROUP BY item_name, item_category, item_subcategory";
+
+    $result = $mysqli->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        echo "<tr><th>Item Name</th><th>Item Category</th><th>Item Subcategory</th><th>Item Quantity</th></tr>";
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["item_name"] . "</td>";
+            echo "<td>" . $row["item_category"] . "</td>";
+            echo "<td>" . $row["item_subcategory"] . "</td>";
+            echo "<td>" . $row["total_quantity"] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "No items found in the database.";
+    }
+
+    $mysqli->close();
+}
+?>
 <!DOCTYPE html>
-<html>
+<html lang="eng">
 <head>
     <link rel="stylesheet" type="text/css" href="style/index.css">
     <title>Item Report</title>
@@ -7,7 +42,7 @@
 <body>
 <?php //include "index.php"; ?>
 <h2>Item Report</h2>
-<form action="/generate_item_report" method="post">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     <input type="submit" value="Generate Report">
 </form>
 </body>
