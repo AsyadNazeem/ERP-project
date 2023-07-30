afbf812995c18237fa431ae42ae85e3effe43bc2
asyadnazeem <asyadnazeem@gmail.com>
2023-07-30 22:25:01 +0530

    styling 90% done

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-// functions.php
-
-function isLinkActive($page) {
-    $currentURL = $_SERVER['PHP_SELF']; // Get the current page URL
-
-    // Check if the current page URL matches the given page link
-    return (strpos($currentURL, $page) !== false);
-}
